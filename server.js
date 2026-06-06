519af49eeb787b1e8a2dd2922c3bb332fc0af535
Olegus-0404 <grigorevoleg2013@gmail.com>
2026-06-06 18:17:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,52 @@ const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
-// Подключаем наш класс игрока из соседнего файла player.js
-const Player = require('./player');
-
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST"] }
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
+
+// КЛАСС ИГРОКА ОПИСАН ПРЯМО ТУТ ДЛЯ НАДЕЖНОСТИ
+class Player {
+    constructor(id, nick) {
+        this.id = id;
+        this.nick = nick;
+        this.hp = 100;
+        this.kills = 0;
+        this.x = (Math.random() * 40) - 20;
+        this.z = (Math.random() * 40) - 20;
+        this.rotY = 0;
+    }
+    updatePosition(x, z, rotY) {
+        if (this.hp > 0) {
+            this.x = x;
+            this.z = z;
+            this.rotY = rotY;
+        }
+    }
+    takeDamage(zone) {
+        if (this.hp <= 0) return false;
+        let damage = (zone === 'head') ? 100 : 20;
+        this.hp -= damage;
+        if (this.hp < 0) this.hp = 0;
+        return this.hp === 0;
+    }
+    respawn() {
+        this.hp = 100;
+        this.x = (Math.random() * 40) - 20;
+        this.z = (Math.random() * 40) - 20;
+        this.rotY = 0;
+    }
+}
 
 let players = {};
 let accounts = {}; 
-
 const MATCH_DURATION = 180; 
 let timeLeft = MATCH_DURATION;
 
@@ -28,15 +60,15 @@ setInterval(() => {
 
 function resetMatch() {
     for (let id in players) {
-        players[id].resetKills(); // Используем метод из класса
-        players[id].respawn();    // Используем метод из класса
+        players[id].kills = 0;
+        players[id].respawn();
         io.to(id).emit('init', { x: players[id].x, z: players[id].z });
     }
     io.emit('updatePlayers', players);
 }
 
 io.on('connection', (socket) => {
-    console.log(`Пользователь подключился: ${socket.id}`);
+    console.log(`Подключился сокет: ${socket.id}`);
     socket.emit('timerUpdate', { timeLeft });
 
     socket.on('playerAuth', (data) => {
@@ -44,7 +76,7 @@ io.on('connection', (socket) => {
         const pass = data.pass ? data.pass.trim() : "";
 
         if (nick.length < 2 || pass.length < 3) {
-            socket.emit('authFailed', "Слишком короткий ник или пароль!");
+            socket.emit('authFailed', "Короткий ник или пароль!");
             return;
         }
 
@@ -59,14 +91,12 @@ io.on('connection', (socket) => {
 
         for (let id in players) {
             if (players[id].nick === nick) {
-                socket.emit('authFailed', "Этот игрок уже на арене!");
+                socket.emit('authFailed', "Ник уже занят на арене!");
                 return;
             }
         }
 
-        // КРАСИВО: Создаем игрока через наш класс!
         players[socket.id] = new Player(socket.id, nick);
-
         socket.emit('authSuccess', { nick, pass });
         socket.emit('init', { x: players[socket.id].x, z: players[socket.id].z });
         io.emit('updatePlayers', players);
@@ -74,7 +104,6 @@ io.on('connection', (socket) => {
 
     socket.on('playerMove', (data) => {
         if (players[socket.id]) {
-            // Используем метод класса для обновления позиции
             players[socket.id].updatePosition(data.x, data.z, data.rotY);
             socket.broadcast.emit('updatePlayers', players);
         }
@@ -83,23 +112,17 @@ io.on('connection', (socket) => {
     socket.on('playerHit', (data) => {
         const shooter = players[socket.id];
         const target = players[data.targetId];
-
         if (!shooter || shooter.hp <= 0 || !target || target.hp <= 0) return;
 
-        // Используем метод класса для нанесения урона
-        const isDead = target.takeDamage(data.zone);
-
-        if (isDead) {
+        if (target.takeDamage(data.zone)) {
             shooter.kills++;
-            console.log(`${shooter.nick} убил ${target.nick}`);
         }
-
         io.emit('updatePlayers', players);
     });
 
     socket.on('requestRespawn', () => {
         if (players[socket.id] && players[socket.id].hp <= 0) {
-            players[socket.id].respawn(); // Оживляем через класс
+            players[socket.id].respawn();
             socket.emit('init', { x: players[socket.id].x, z: players[socket.id].z });
             io.emit('updatePlayers', players);
         }
@@ -107,7 +130,6 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         if (players[socket.id]) {
-            console.log(`Игрок ${players[socket.id].nick} покинул арену.`);
             delete players[socket.id];
             io.emit('updatePlayers', players);
         }
@@ -115,6 +137,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Сервер активен на порту ${PORT}`));
